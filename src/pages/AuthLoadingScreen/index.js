1af4190fadbc363136fd9f6dd7b16b981b07faa9
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-03 15:46:13 -0300

    Authloading page added

---
+++ src/pages/AuthLoadingScreen/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {
+    ActivityIndicator,
+    StyleSheet,
+    View
+} from 'react-native'
+import style from './style'
+import { white } from '../../utils/colors';
+import { connect } from 'react-redux'
+
+class AuthLoadingScreen extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this._bootstrapAsync();
+    }
+
+    _bootstrapAsync = async () => {
+
+        const { user } = this.props
+
+        // GET LOGGED USER
+        const USER = user
+
+        this.props.navigation.navigate(USER.isLogged ? 'App' : 'Auth')
+
+
+    }
+
+    render() {
+        return (
+            <View style={[styles.container, styles.horizontal]}>
+                <ActivityIndicator size="large" color={white} />
+            </View>
+        )
+    }
+
+}
+
+const styles = StyleSheet.create(style)
+
+const mapStateToProps = ({ user }) => {
+    return {
+        user
+    }
+}
+
+export default connect(
+    mapStateToProps
+)(AuthLoadingScreen)
---
+++ src/pages/AuthLoadingScreen/style.js
@@ -0,0 +1,14 @@
+import { purple } from "../../utils/colors";
+
+export default ({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: purple
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10
+    }
+})
