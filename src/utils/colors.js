b48dbbb0faaccbae700985ecdd9e2d2c80afae45
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-07 23:11:41 -0300

    new colors added

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -3,5 +3,11 @@ export default COLORS = {
     gray: '#E4E4E4',
     dark_gray: '#A2A2A2',
     orange:  '#FF6B00',
-    light_orange:  '#FF9A51'
+    light_orange:  '#FF9A51',
+    text_gray: '#999999',
+    green: '#24A641',
+    background_purple: '#DFDEEE',
+    step_indicator_purple: '#6455A3',
+    red: '#ff0000',
+    modal_background: '#00000095'
 }
