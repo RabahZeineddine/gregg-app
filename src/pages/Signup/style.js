0a2d6480bb76120afdd2b2a57adec2a05d021d14
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-07 23:12:04 -0300

    signup style

---
+++ src/pages/Signup/style.js
@@ -0,0 +1,33 @@
+import colors from "../../utils/colors";
+
+export default ({
+    container: {
+        flex: 1
+    },
+    holder: {
+        flex: 1,
+        paddingTop: 32,
+
+    },
+    titleHolder: {
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 16
+    },
+    title: {
+        color: colors.white,
+        fontSize: 28,
+        fontFamily: 'Overpass-ExtraBold',
+        fontWeight: 'bold'
+    },
+    viewPager: {
+        flexGrow: 1
+    },
+    pageContainer: {
+        flex: 1,
+        paddingTop: 16,
+        paddingLeft: 20,
+        paddingRight: 20
+
+    }
+}) 
