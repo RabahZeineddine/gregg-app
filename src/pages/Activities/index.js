f646bf27b5782b5e48a0adb4e9377a988496f80b
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-23 23:41:02 -0300

    Activities page added

---
+++ src/pages/Activities/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { View, Text } from 'react-native'
+
+
+class Activities extends React.Component {
+
+    render() {
+        return (<View>
+            <Text>Activities</Text>
+        </View>)
+    }
+}
+
+export default Activities
