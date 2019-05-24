c116eeaef32a241035e2be0f48dfaf328327d4e9
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-23 23:39:42 -0300

    new colors added

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -2,12 +2,17 @@ export default COLORS = {
     white: '#fff',
     gray: '#E4E4E4',
     dark_gray: '#A2A2A2',
-    orange:  '#FF6B00',
-    light_orange:  '#FF9A51',
+    orange: '#FF6B00',
+    light_orange: '#FF9A51',
     text_gray: '#999999',
     green: '#24A641',
     background_purple: '#DFDEEE',
     step_indicator_purple: '#6455A3',
     red: '#ff0000',
-    modal_background: '#00000095'
+    modal_background: '#00000095',
+    white_background: '#F9F9F9',
+    menu_item_color: '#707070',
+    title_gray: '#696969',
+    title_border_gray: '#E2E2E2',
+    red: '#E94242'
 }
