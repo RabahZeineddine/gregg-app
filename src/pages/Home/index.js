4ea0cb87441f0e81c747d186ad44599b0e7dc63d
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-23 23:40:26 -0300

    Home page added

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {
+    View,
+    StyleSheet,
+    ScrollView
+} from 'react-native'
+
+import style from './style'
+import ProfileComponent from '../../components/ProfileComponent';
+import CouponsComponent from '../../components/CouponsComponent';
+import ActivitiesComponent from '../../components/ActivitiesComponent';
 
 class Home extends React.Component {
 
+    static navigationOptions = {
+        drawerLabel: 'Início'
+    }
     render() {
         return (
-            <View>
-                <Text>Home</Text>
+            <View style={styles.container} >
+                <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+                    <ProfileComponent />
+                    <CouponsComponent navigation={this.props.navigation} />
+                    <ActivitiesComponent navigation={this.props.navigation} />
+                </ScrollView>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create(style)
 
 export default Home
---
+++ src/pages/Home/style.js
@@ -0,0 +1,13 @@
+import colors from "../../utils/colors";
+
+export default ({
+    container: {
+        flexGrow: 1,
+        backgroundColor: colors.white_background,
+    },
+    contentContainer: {
+        flexGrow: 1,
+        backgroundColor: colors.white_background,
+        padding: 20,
+    }
+})
