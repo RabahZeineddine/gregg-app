580df96ab68a20c0aa2423d6f265331b53715574
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-28 00:35:13 -0300

    Logout page added

---
+++ src/pages/Logout/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native'
+
+import style from './style'
+
+class Logout extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text>logout</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create(style)
+
+export default Logout
